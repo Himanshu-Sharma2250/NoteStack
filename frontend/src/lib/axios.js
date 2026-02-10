cf2d9ec684985ea7ec67049599e1e7c918de1630
Himanshu-Sharma2250 <him221901250@gmail.com>
2026-02-10 19:27:16 +0530

    add deployed backend url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api/v1",
+    baseURL: "https://notestack-backend-sdoj.onrender.com/api/v1",
     withCredentials: true
 })
